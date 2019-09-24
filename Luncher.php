2e164e977a6e808d4d91cb4f3e53ffc61899a05f
lovelorn1364 <55733190+lovelorn1364@users.noreply.github.com>
2019-09-24 13:56:45 +0330

    Update Luncher.php

--- Luncher.php
+++ Luncher.php
@@ -1,5 +1,5 @@
 <?php
-define('BOT_TOKEN', '**TOKEN**');
+define('BOT_TOKEN', '439432957:AAEknKYAumtFwdxYEbEn9fFT170WTUnCsO8');
 define('API_URL', 'https://api.telegram.org/bot'.BOT_TOKEN.'/');
 function apiRequestWebhook($method, $parameters) {
 
@@ -905,7 +905,7 @@ function apiRequestWebhook($method, $parameters) {
     
     
       
-      $admin = **ADMIN**;
+      $admin = 54855833;
 
 
 
